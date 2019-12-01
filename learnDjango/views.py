5dcfe80a297eb17fe1fe2fbfa19579d427aeff84
= <=>
2019-11-30 21:50:34 -0600

    Creando primer template

--- learnDjango/views.py
+++ learnDjango/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
 
 def saludo(request):
-  return HttpResponse('Hello World')
+  doc_externo=open('/var/www/html/proyectos-django/learnDjango/learnDjango/templates/home.html')
+  plt=Template(doc_externo.read())
+  doc_externo.close()
+  ctx=Context()
+  document=plt.render(ctx)
+  return HttpResponse(document)
 
 def date(request):
   current_date=datetime.datetime.now()
